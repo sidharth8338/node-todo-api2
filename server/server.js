b1e2366445c865e0a80deca48fe5dc879631c603
SIKU <sidharth8338@gmail.com>
2019-02-15 15:48:14 +0530

    GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const { mongoose } = require('./db/mongoose')
 const { Users } = require('./models/Users')
 const { Todo } = require('./models/todo')
+const { ObjectID } = require('mongodb')
 
 let app = express()
 app.use(bodyParser.json())
@@ -25,6 +26,20 @@ app.get('/todos', (req, res) => {
         res.status(404).send(e)
     })
 })
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send({})
+    }
+    Users.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send({})
+        }
+        res.send({ todo })
+    }).catch((e) => res.status(400).send())
+})
+
 app.listen(3000, () => {
     console.log('started at port 3000')
 })
